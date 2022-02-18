e4cbcb12540f59f7e511cd3da207c18770f8c6d9
Tiago Dias <tiagojpdias@gmail.com>
2022-02-18 12:14:28 +0000

    fix: map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,8 @@
 import L from 'leaflet';
+import { useMemo } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useStations } from '../context/App'
+import { mapCenter } from '../utils/constants';
 
 const LeafIcon = L.Icon.extend({
       options: {
@@ -33,30 +35,30 @@ L.Icon.Default.mergeOptions({
 export const Map = ({ selectedLine }) => {
   const stations = useStations();
 
+  const markers = useMemo(() => [...stations.values()]
+    .filter(({ line = [] }) => line.includes(selectedLine))
+    .map(marker => ({
+      ...marker,
+      icon: marker.line.length > 1 ? iconsMap.default : iconsMap[marker.line[0]],
+    })), [stations, selectedLine]);
+
   if (!stations.size) {
     return null;
   }
 
-  const markers = [...stations.values()]
-    .filter(({ line = [] }) => line ? line.includes(selectedLine) : true)
-    .map(marker => ({
-      ...marker,
-      icon: marker.line?.length > 1 ? iconsMap.default : iconsMap[marker.line[0]],
-    }));
-
   return (
     <div className="row">
-      <MapContainer zoom={12} center={[markers[0]?.position.lat, markers[0]?.position.lon]} style={{ height: '98vh' }}>
+      <MapContainer zoom={12} center={mapCenter} style={{ height: '98vh' }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         { markers.map(({ position, name, icon, line }, index) => (
-          position?.lat ? (
+          (
             <Marker key={index} position={[position.lat, position.lon]} icon={icon}>
               <Popup>
                 <div>{ name }</div>
                 <div>Line color: {line.join(' and ')}</div>
               </Popup>
             </Marker>
-          ) : null
+          )
         ))
       }
       </MapContainer>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,5 @@ export const SECONDS = 1000;
 
 export const baseUrl = 'https://diconium-lisbon-job-fairs.herokuapp.com';
 
+export const mapCenter =[38.736946, -9.142685]; // Lisbon
+
